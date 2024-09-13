edbe73618b9225829dac6b835b38da4675dd7313
leerlandais <leerlandais@gmail.com>
2024-09-13 10:05:43 +0200

    added relation: Post->Section

---
+++ migrations/Version20240913073237.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240913073237 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE comment (id INT UNSIGNED AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE section (id INT UNSIGNED AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE tag (id INT UNSIGNED AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE post CHANGE id id INT UNSIGNED AUTO_INCREMENT NOT NULL, CHANGE post_date_created post_date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE post_is_published post_is_published TINYINT(1) DEFAULT 0 NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE comment');
+        $this->addSql('DROP TABLE section');
+        $this->addSql('DROP TABLE tag');
+        $this->addSql('ALTER TABLE post CHANGE id id INT AUTO_INCREMENT NOT NULL, CHANGE post_date_created post_date_created DATETIME NOT NULL, CHANGE post_is_published post_is_published TINYINT(1) NOT NULL');
+    }
+}
--- src/Entity/Section.php
+++ src/Entity/Section.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\SectionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SectionRepository::class)]
@@ -17,8 +19,76 @@ class Section
     )]
     private ?int $id = null;
 
+    #[ORM\Column(length: 120)]
+    private ?string $sectionTitle = null;
+
+    #[ORM\Column(length: 600, nullable: true)]
+    private ?string $section_Description = null;
+
+    /**
+     * @var Collection<int, Post>
+     */
+    #[ORM\ManyToMany(targetEntity: Post::class, mappedBy: 'sections')]
+    private Collection $posts;
+
+    public function __construct()
+    {
+        $this->posts = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getSectionTitle(): ?string
+    {
+        return $this->sectionTitle;
+    }
+
+    public function setSectionTitle(string $sectionTitle): static
+    {
+        $this->sectionTitle = $sectionTitle;
+
+        return $this;
+    }
+
+    public function getSectionDescription(): ?string
+    {
+        return $this->section_Description;
+    }
+
+    public function setSectionDescription(?string $section_Description): static
+    {
+        $this->section_Description = $section_Description;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->addSection($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            $post->removeSection($this);
+        }
+
+        return $this;
     }
 }
