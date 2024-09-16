e393a54cbcdcb19059420290c9b1d704340586e8
leerlandais <leerlandais@gmail.com>
2024-09-16 16:04:11 +0200

    hometime

--- src/Form/SectionType.php
+++ src/Form/SectionType.php
@@ -2,9 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Post;
 use App\Entity\Section;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,11 +14,8 @@ class SectionType extends AbstractType
         $builder
             ->add('sectionTitle')
             ->add('section_Description')
-            ->add('posts', EntityType::class, [
-                'class' => Post::class,
-                'choice_label' => 'id',
-                'multiple' => true,
-            ])
+
+
         ;
     }
 
