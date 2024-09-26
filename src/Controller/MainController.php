940310b20a044d49e35a3c6ad58f566b3aece599
leerlandais <leerlandais@gmail.com>
2024-09-26 12:13:17 +0200

    lunchtime

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,19 +10,37 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(SectionRepository $sections): Response
     {
         return $this->render('main/index.html.twig', [
             'title' => 'Homepage',
-            'homepage_text' => "Nous sommes le".date("d/m/Y \à H:i e-GMT")
+            'homepage_text' => "Nous sommes le".date("d/m/Y \à H:i e-GMT"),
+            'sections' => $sections->findAll()
         ]);
     }
     #[Route('/about', name: 'about')]
-    public function about(): Response
+    public function about(SectionRepository $sections): Response
     {
         return $this->render('main/about.html.twig', [
             'title' => 'About',
-            'homepage_text' => 'Code, Eat, Sleep, Repeat'
+            'homepage_text' => 'Code, Eat, Sleep, Repeat',
+            'sections' => $sections->findAll()
         ]);
     }
-}
+
+    #[Route(
+        path: '/section/{id}',
+        name: 'section',
+        requirements: ['id' => '\d+'],
+        defaults: ['id' => 1])]
+    public function section(SectionRepository $sections, int $id): Response
+    {
+        $section = $sections->find($id);
+        return $this->render('main/section.html.twig',
+            ['title' => 'Section '.$section->getSectionTitle(),
+                'section' => $sections->find($id),
+                'sections' => $sections->findAll()]);
+    }
+
+
+} // end of Class
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-
+use App\Repository\SectionRepository;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SectionRepository $sections): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('homepage');
@@ -24,6 +24,7 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
+            'sections' => $sections->findAll()
         ]);
     }
 
